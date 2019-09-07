aca23f689abb0de6f0bd06d8ed6a148dcca6c57c
Damian Duarte <damianleo1991@hotmail.com>
2019-09-06 23:07:44 -0500

    11:07

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { async } from 'q';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,28 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  textoEntrada: string;
+  nombres: any[];
+  nombreResultado: string;
+
+
+  constructor() { }
+
+  public ruleta() {
+    this.nombres = this.textoEntrada.split(',');
+    console.log(this.nombres);
+    for (const nombre of this.nombres) {
+      const i: number = Math.floor(Math.random() * this.nombres.length);
+      console.log(this.nombreResultado);
+      const seg = formatDate(Date.now(), 's', 'en-US').valueOf();
+      while ( (seg + 1) > formatDate(Date.now(), 's', 'en-US').valueOf() ) {
+
+        this.nombreResultado = this.nombres[i];
+      }
+    }
+  }
+
 
 }
+
+
